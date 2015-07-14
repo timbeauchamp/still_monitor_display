dfabf738aa898e70e7bbd563f1f5b56a7eceb0b1
Tim Beauchamp <tbeauch@adobe.com>
2015-07-13 17:55:47 -0700

    tweaks

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,22 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -24,7 +25,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import sun.misc.IOUtils;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -34,22 +34,62 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main extends Application
 {
-    Text resultText;
 
-    double[] temps;
-    double[] ranges;
-    boolean[] switches;
+    public static String dataURL = "http://localhost://temps.xml"; // http://192.168.1.15/temps.xml;
+    Text resultText;
+    RemoteData rd = new RemoteData();
 
     Label[] tempControl = new Label[3];
     Label[] rangeControl = new Label[3];
     Label[] switchControl = new Label[3];
 
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception
+    {
+        buildUI(primaryStage);
+        primaryStage.show();
+
+        DataRetrievalService service = new DataRetrievalService();
+        service.setUrl(dataURL);
+        service.setOnSucceeded(new EventHandler<WorkerStateEvent>()
+        {
+            @Override
+            public void handle(WorkerStateEvent t)
+            {
+                RemoteData rd = (RemoteData) t.getSource().getValue();
+                System.out.println("done:" + rd.toString());
+                updateUI(rd);
+            }
+        });
+        service.start();
+        startUpdater();
+
+    }
+
+    public void startUpdater()
+    {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run()
+            {
+                // Update the text node with calculated results
+            }
+        });
+    }
+
+
+    private void buildUI(Stage primaryStage)
     {
         BorderPane root = new BorderPane();
         primaryStage.setTitle("Still Monitor Display");
@@ -61,10 +101,19 @@ public class Main extends Application
             @Override
             public void handle(ActionEvent event)
             {
-                System.out.println("Update Temps");
-                resultText.setText("Getting Data");
-                // should do this on a different thread
-                getUpdate("http://192.168.1.15/temps.xml");
+                DataRetrievalService service = new DataRetrievalService();
+                service.setUrl(dataURL);
+                service.setOnSucceeded(new EventHandler<WorkerStateEvent>()
+                {
+                    @Override
+                    public void handle(WorkerStateEvent t)
+                    {
+                        RemoteData rd = (RemoteData) t.getSource().getValue();
+//                        System.out.println("done:" + rd.toString());
+                        updateUI(rd);
+                    }
+                });
+                service.start();
             }
         });
 
@@ -93,11 +142,11 @@ public class Main extends Application
         grid.add(tempControl[1], 1, 4);
         grid.add(tempControl[2], 1, 5);
 
-        grid.add(rangeControl[0],2,3);
+        grid.add(rangeControl[0], 2, 3);
         grid.add(rangeControl[1], 2, 4);
-        grid.add(rangeControl[2],2,5);
+        grid.add(rangeControl[2], 2, 5);
 
-        grid.add(switchControl[0],3, 3);
+        grid.add(switchControl[0], 3, 3);
         grid.add(switchControl[1], 3, 4);
         grid.add(switchControl[2], 3, 5);
 
@@ -132,133 +181,159 @@ public class Main extends Application
 
         Scene scene = new Scene(grid, 600, 475);
         primaryStage.setScene(scene);
-        primaryStage.show();
-
-
-
     }
 
-    private void getUpdate(String urlStr)
+    private static Document getData(String urlStr)
     {
-        URL url = null;
+        URL url;
+        Document doc = null;
         try
         {
-                url = new URL(urlStr);
+            url = new URL(urlStr);
 
         } catch (MalformedURLException e)
         {
             e.printStackTrace();
-            return;
-        } catch (IOException e)
-        {
-            e.printStackTrace();
+            return null;
         }
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = null;
+        DocumentBuilder db;
         try
         {
             db = dbf.newDocumentBuilder();
-        }
-        catch (ParserConfigurationException e)
+        } catch (ParserConfigurationException e)
         {
             e.printStackTrace();
-            return;
+            return null;
         }
         try
         {
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(url.openStream()));
-
-                            String inputLine;
-            while ((inputLine = in.readLine()) != null)
-                System.out.println(inputLine);
-            in.close();
-
-
-                Document doc = db.parse(url.openStream());
-
-            NodeList currentNodeList = doc.getElementsByTagName("temp");
-            int len = currentNodeList.getLength();
-
-            if(len > 0)
-                temps = new double[len];
-
-            Node currentNode;
-            String nodeVal;
-            for(int i = 0 ; i < len; i++)
-            {
-                currentNode = currentNodeList.item(i);
-                nodeVal = currentNode.getFirstChild().getNodeValue();
-                temps[i] = Double.parseDouble(nodeVal);
-            }
-
-            currentNodeList = doc.getElementsByTagName("range");
-            len = currentNodeList.getLength();
-
-            if(len > 0)
-                ranges = new double[len];
-
-            for(int i = 0 ; i < len; i++)
-            {
-                currentNode = currentNodeList.item(i);
-                nodeVal = currentNode.getFirstChild().getNodeValue();
-                ranges[i] = Double.parseDouble(nodeVal);
-            }
-
-            currentNodeList = doc.getElementsByTagName("switch");
-            len = currentNodeList.getLength();
-
-            if(len > 0)
-                switches = new boolean[len];
-
-            for(int i = 0 ; i < len; i++)
-            {
-                currentNode = currentNodeList.item(i);
-                nodeVal = currentNode.getFirstChild().getNodeValue();
-                switches[i] = Boolean.parseBoolean(nodeVal);
-            }
-
-        } catch (SAXException e)
-        {
-            e.printStackTrace();
-        } catch (IOException e)
+            doc = db.parse(url.openStream());
+        } catch (SAXException|IOException e)
         {
             e.printStackTrace();
         }
-        updateUI();
+
+        return doc;
     }
 
-    private void updateUI()
+    private static RemoteData doc2RemoteData(Document doc)
+    {
+        RemoteData rd = new RemoteData();
+        NodeList currentNodeList = doc.getElementsByTagName("temp");
+        int len = currentNodeList.getLength();
+
+        if (len > 0)
+            rd.temps = new double[len];
+
+        Node currentNode;
+        String nodeVal;
+        for (int i = 0; i < len; i++)
+        {
+            currentNode = currentNodeList.item(i);
+            nodeVal = currentNode.getFirstChild().getNodeValue();
+            rd.temps[i] = Double.parseDouble(nodeVal);
+        }
+
+        currentNodeList = doc.getElementsByTagName("range");
+        len = currentNodeList.getLength();
+
+        if (len > 0)
+            rd.ranges = new double[len];
+
+        for (int i = 0; i < len; i++)
+        {
+            currentNode = currentNodeList.item(i);
+            nodeVal = currentNode.getFirstChild().getNodeValue();
+            rd.ranges[i] = Double.parseDouble(nodeVal);
+        }
+
+        currentNodeList = doc.getElementsByTagName("switch");
+        len = currentNodeList.getLength();
+
+        if (len > 0)
+            rd.switches = new boolean[len];
+
+        for (int i = 0; i < len; i++)
+        {
+            currentNode = currentNodeList.item(i);
+            nodeVal = currentNode.getFirstChild().getNodeValue();
+            rd.switches[i] = Boolean.parseBoolean(nodeVal);
+        }
+        return rd;
+     }
+
+    private void updateUI(RemoteData rd)
     {
         int index = 0;
-        for(double temp : temps)
+        for (double temp : rd.temps)
         {
-            if(index > 2)
+            if (index > 2)
                 break;
-            tempControl[index].setText(Double.toString(temps[index++]));
+            tempControl[index].setText(Double.toString(rd.temps[index++]));
         }
 
         index = 0;
-        for(double range : ranges)
+        for (double range : rd.ranges)
         {
-            if(index > 2)
+            if (index > 2)
                 break;
-            rangeControl[index].setText(Double.toString(ranges[index++]));
+            rangeControl[index].setText(Double.toString(rd.ranges[index++]));
         }
 
         index = 0;
-        for(boolean aSwitch : switches)
+        for (boolean aSwitch : rd.switches)
         {
-            if(index > 2)
+            if (index > 2)
                 break;
-            switchControl[index].setText(Boolean.toString(switches[index++]));
+            switchControl[index].setText(Boolean.toString(rd.switches[index++]));
         }
     }
 
-
-    public static void main(String[] args)
+    private static class DataRetrievalService extends Service<RemoteData>
     {
-        launch(args);
+        private StringProperty url = new SimpleStringProperty();
+
+        public final void setUrl(String value)
+        {
+            url.set(value);
+        }
+
+        public final String getUrl()
+        {
+            return url.get();
+        }
+
+        public final StringProperty urlProperty()
+        {
+            return url;
+        }
+
+        @Override
+        protected Task<RemoteData> createTask()
+        {
+            return new Task<RemoteData>()
+            {
+                @Override
+                protected RemoteData call() throws IOException, MalformedURLException
+                {
+                    return doc2RemoteData(getData(getUrl()));
+                }
+            };
+        }
+    }
+
+    private static class RemoteData
+    {
+        public double[] temps = new double[0];
+        public double[] ranges = new double[0];
+        public boolean[] switches = new boolean[0];
+
+        @Override
+        public String toString()
+        {
+            return "Num Temps:"+ temps.length + "  Num Ranges:"+ ranges.length + "  Num Swithces: "+ switches.length;
+        }
     }
 }
